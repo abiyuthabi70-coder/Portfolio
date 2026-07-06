f9acf0280b20e8b10e92610f6a776ed654dd26d8
abiyuthabi70-coder <abiyuthabi70@gmail.com>
2026-07-06 21:37:04 +0530

    Add Skills component to display skillset

---
+++ Skills.jsx
@@ -0,0 +1,16 @@
+function Skills() {
+  return (
+    <section>
+      <h2>Skills</h2>
+
+      <div className="skills">
+        <div>HTML</div>
+        <div>CSS</div>
+        <div>JavaScript</div>
+        <div>React</div>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
