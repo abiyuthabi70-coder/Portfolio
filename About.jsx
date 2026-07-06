c76e6844d6566a1fcee3c155dfca640b5c2ebc33
abiyuthabi70-coder <abiyuthabi70@gmail.com>
2026-07-06 21:36:13 +0530

    Add About component with personal description

---
+++ About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        Passionate Frontend Developer skilled in HTML, CSS,
+        JavaScript and React.
+      </p>
+    </section>
+  );
+}
+
+export default About;
