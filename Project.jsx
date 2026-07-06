4d06f2b7fa2d5e7c34992dd4d0424511bacef154
abiyuthabi70-coder <abiyuthabi70@gmail.com>
2026-07-06 21:39:18 +0530

    Create Project.jsx

---
+++ Project.jsx
@@ -0,0 +1,23 @@
+function Projects() {
+  return (
+    <section>
+      <h2>Projects</h2>
+
+      <div className="projects">
+
+        <div className="card">
+          <h3>Portfolio</h3>
+          <p>Built using HTML CSS</p>
+        </div>
+
+        <div className="card">
+          <h3>Weather App</h3>
+          <p>Built using React</p>
+        </div>
+
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
