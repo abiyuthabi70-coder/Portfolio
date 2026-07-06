082db14ee9724769cafc2aeaa1578ac63734e4d6
abiyuthabi70-coder <abiyuthabi70@gmail.com>
2026-07-06 21:41:47 +0530

    Create Contact component with form inputs

---
+++ contact.jsx
@@ -0,0 +1,17 @@
+function Contact() {
+  return (
+    <section>
+      <h2>Contact</h2>
+
+      <input type="text" placeholder="Name" />
+
+      <input type="email" placeholder="Email" />
+
+      <textarea placeholder="Message"></textarea>
+
+      <button>Send</button>
+    </section>
+  );
+}
+
+export default Contact;
