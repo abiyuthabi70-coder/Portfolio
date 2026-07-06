357d29d33a383e623a57e3d3ada5ab839369a8e7
abiyuthabi70-coder <abiyuthabi70@gmail.com>
2026-07-06 21:38:32 +0530

    Create Hero.jsx

---
+++ Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Hello, I'm John Doe</h1>
+      <h3>Frontend Developer</h3>
+      <button>Download Resume</button>
+    </section>
+  );
+}
+
+export default Hero;
